33afa112bc331feb5f32d5359800019db6aedb5f
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-10-10 13:05:09 -0500

    Send email with nodemailer

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,6 +2,7 @@ import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDatasource } from "../infraestructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infraestructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+import { EmailSerive } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource()
@@ -12,6 +13,16 @@ export class Server {
     console.log("Server started...");
 
     // Enviar email
+    const emailService = new EmailSerive();
+    emailService.sendEmail({
+      to: "alfonso_gonzalezg@outlook.com",
+      subject: "Logs de sistema",
+      htmlBody: `
+        <h3>Logs de ssitema - NOC</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent venenatis mollis risus viverra convallis. Maecenas eu turpis vehicula, maximus nulla at, iaculis mi.</p>
+        <p>Ver logs adjuntos</p>
+      `,
+    });
 
     // CronService.createJob("*/5 * * * * *", () => {
     //   const url = "https://google.com";
