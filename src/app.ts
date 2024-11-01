1f3ed33ad07fcbca7d00f54dd21764275eeba5f9
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2024-11-01 12:24:18 -0500

    Using prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import { PrismaClient } from "@prisma/client";
 import { envs } from "./config/plugins/envs.plugin";
 import { LogModel, MongoDatabase } from "./data/mongo";
 import { Server } from "./presentation/server";
@@ -11,6 +12,20 @@ async function main() {
     mongoUrl: envs.MONGO_URL,
     dbName: envs.MONGO_DB_NAME,
   });
+
+  // const prisma = new PrismaClient();
+  // const newLog = await prisma.logModel.create({
+  //   data: {
+  //     level: "HIGH",
+  //     message: "Test message",
+  //     origin: "App.ts",
+  //   },
+  // });
+  // const logs = await prisma.logModel.findMany({
+  //   where: { level: "MEDIUM" },
+  //   orderBy: { id: "asc" },
+  // });
+  // console.log("🚀 ~ main ~ logs:", logs);
 
   Server.start();
 }
